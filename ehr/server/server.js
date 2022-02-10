d0e8313bcffec735fe11e538b7cdf44c33f3aa42
Chris Blaylock <52610450+blaylockc09@users.noreply.github.com>
2022-02-10 08:34:22 -0500

    adding routes and sequelize

--- ehr/server/server.js
+++ ehr/server/server.js
@@ -1,16 +1,19 @@
 const express = require("express");
-// const bodyParser = require("body-parser"); /* deprecated - as of version 4.16+ express now has body-parser built in*/
-const cors = require("cors");
-
+const mysql = require('mysql');
+const cors = require('cors');
 const app = express();
 
-//Configures the Access-Control-Allow-Origin CORS header.
+const sequelize = require('./app/utils/database');
+const User = require("./app/models/User.model");
+
+/* //Configures the Access-Control-Allow-Origin CORS header.
 var corsOptions = {
   origin: "http://localhost:8081"
-};
+}; */
 
-app.use(cors(corsOptions));
+/* app.use(cors(corsOptions)); */
 
+app.use(cors());
 // parse requests of content-type - application/json
 app.use(express.json()); /* bodyParser.json() is deprecated */
 
@@ -21,11 +24,42 @@ app.use(express.urlencoded({ extended: true })); /* bodyParser.urlencoded() is d
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Clark State EHR" });
 });
-
-require("./app/routes/tutorial.routes.js")(app);
-
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+const PORT = process.env.PORT || 3001;
+//require("./app/routes/tutorial.routes.js")(app);
+const userRoutes = require("./app/routes/users.routes");
+
+const databaseTest = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'password',
+  database: 'clark_state_ehr'
 });
+
+app.use(userRoutes);
+
+
+app.delete('/user/delete', (req, res) => {
+  const user_id = req.body.user_id;
+  databaseTest.query(
+      'DELETE FROM users WHERE user_id = ?', 
+      [user_id],
+      (err, result) => {
+          if(err){
+              console.log(err);
+          } else {
+              console.log("User Deleted");
+          }
+      }
+  ); // insert array and '?' for security purposes
+});
+
+sequelize.sync().then(result =>{
+    console.log(result);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  })
+  .catch(err => {
+      console.log(err);
+  });
