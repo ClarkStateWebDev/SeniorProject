8ef0dd3e9c83e6cd06c2d58e7398beecff383ce5
Alex2point0 <18444027+Alex2point0@users.noreply.github.com>
2022-03-10 16:57:54 -0500

    Revert "Jeris code"

--- ehr/client/src/pages/Unauthorized.js
+++ ehr/client/src/pages/Unauthorized.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const Unauthorized = () => {
     return (
@@ -10,17 +9,4 @@ const Unauthorized = () => {
     )
 }
 
-=======
-
-const Unauthorized = () => {
-    return (
-        <section>
-            <h1>Unauthorized</h1>
-            <br />
-            <p>You do not have access to the requested page.</p>
-        </section>
-    )
-}
-
->>>>>>> c2169ba77d2dece1f9eeec06a233b2ede44b7181
 export default Unauthorized
--- ehr/client/src/pages/login/loggedOut.js
+++ ehr/client/src/pages/login/loggedOut.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useNavigate  } from "react-router-dom"
 import Button from 'react-bootstrap/Button';
 
@@ -13,20 +12,4 @@ const LoggedOut = () => {
     )
 }
 
-=======
-import { useNavigate  } from "react-router-dom"
-import Button from 'react-bootstrap/Button';
-
-const LoggedOut = () => {
-    const navigate = useNavigate();
-    return (
-        <article style={{ padding: "100px" }}>
-            <h1>You have logged out of your account</h1>
-            <p>Please close your browser.</p>
-            
-        </article>
-    )
-}
-
->>>>>>> c2169ba77d2dece1f9eeec06a233b2ede44b7181
 export default LoggedOut
